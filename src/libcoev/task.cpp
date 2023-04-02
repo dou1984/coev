b4ed12317f91f807a2fe2ffe50cf905e2cfaa128
Zhao YunShan <dou1984@163.com>
2023-04-02 22:23:20 +0800

    update
    update

--- src/libcoev/task.cpp
+++ src/libcoev/task.cpp
@@ -14,7 +14,6 @@ namespace coev
 	{
 		if (m_taskchain)
 		{
-			TRACE();
 			m_taskchain->EVTask::erase(this);
 			m_taskchain->EVEvent::resume_ex();
 			m_taskchain = nullptr;
@@ -28,13 +27,11 @@ namespace coev
 	{
 		EVTask::push_back(_task);
 		_task->m_taskchain = this;
-		TRACE();
 	}
 	void taskchain::destroy()
 	{
 		while (!EVTask::empty())
 		{
-			TRACE();
 			auto c = static_cast<task *>(EVTask::pop_front());
 			c->taskext::m_taskchain = nullptr;
 			c->destroy();
