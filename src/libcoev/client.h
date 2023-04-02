080256ed9f8f1ef8c401c0371edda24bf70d5219
Zhao YunShan <dou1984@163.com>
2023-04-02 22:16:36 +0800

    update
    update

--- src/libcoev/client.h
+++ src/libcoev/client.h
@@ -13,14 +13,17 @@
 #include "iocontext.h"
 
 namespace coev
-{	
-	struct client : iocontext
-	{		
+{
+	class client final : public iocontext
+	{
+	public:
 		client();
 		virtual ~client();
-		int connect(const char *ip, int port);	
+		int connect(const char *ip, int port);
+
+	private:
 		int connect_insert();
-		int connect_remove();	
+		int connect_remove();
 		int __connect(int fd, const char *ip, int port);
 		static void cb_connect(struct ev_loop *loop, struct ev_io *w, int revents);
 	};
