e28bc97165212e0b305e78fd0e93912cf7571ede
Zhao YunShan <dou1984@163.com>
2024-10-01 15:37:58 +0800

    update
    update

--- examples/cosys/coloop.h
+++ examples/cosys/coloop.h
@@ -2,11 +2,7 @@
 
 #include <coev.h>
 #include "coloop/coloop.h"
-#include "coloop/client.h"
-#include "coloop/server.h"
-#include "coloop/serverpool.h"
 #include "coloop/sleepfor.h"
 #include "coloop/running.h"
-#include "coloop/udp.h"
 #include "coloop/timer.h"
 
